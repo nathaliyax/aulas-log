5d81889cea65bb3a2f161daed83030b1797adce4
nathaliyax <nathalialimas@hotmail.com>
2025-02-22 15:24:52 -0300

    Update aula2.java

--- nljava/aula2.java
+++ nljava/aula2.java
@@ -5,9 +5,7 @@ import java.util.Scanner;
 public class aula2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.print("hello world");
-		
+		System.out.print("hello world");	
 	}
 
 }
